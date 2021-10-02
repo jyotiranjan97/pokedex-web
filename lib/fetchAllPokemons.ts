0d14126536d04162b98f386b6377b7ad9c96c299
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-10-02 17:18:11 +0530

    added loadmore and scroll to top

--- lib/fetchAllPokemons.ts
+++ lib/fetchAllPokemons.ts
@@ -1,10 +1,29 @@
+const URL = "https://pokeapi.co/api/v2/pokemon";
+
 export const fetchAllPokemons = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const res = await fetch(URL + "?limit=20");
   const { results } = await res.json();
   const pokemons = results.map((result, index) => {
     const pokeIndex = ("000" + (index + 1)).slice(-4);
     const pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
       index + 1
+    }.svg`;
+    return {
+      pokeIndex,
+      ...result,
+      pokeImage,
+    };
+  });
+  return pokemons;
+};
+
+export const fetchMorePokemons = async (offset: number) => {
+  const res = await fetch(URL + `?limit=20&offset=${offset}`);
+  const { results } = await res.json();
+  const pokemons = results.map((result, index) => {
+    const pokeIndex = ("000" + (index + 1 + offset)).slice(-4);
+    const pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+      index + 1 + offset
     }.svg`;
     return {
       pokeIndex,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,46 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import Results from "../components/Results/Results";
 import SearchBox from "../components/SearchBox/SearchBox";
-import { fetchAllPokemons } from "../lib/fetchAllPokemons";
+import { fetchAllPokemons, fetchMorePokemons } from "../lib/fetchAllPokemons";
+import scroll from "../public/Scroll.svg";
 
 export default function Home({ pokemons }) {
+  const [allPokemons, setAllPokemons] = useState(pokemons);
+  const [offset, setOffset] = useState(1);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const loadMorePokemons = async () => {
+    const newOffset = offset + 20;
+    console.log(newOffset);
+    const newPokemons = await fetchMorePokemons(newOffset);
+    setOffset(newOffset);
+    setAllPokemons((pokemons) => [...pokemons, ...newPokemons]);
+  };
+
   return (
     <div className="flex flex-col items-center dark:bg-black justify-center min-h-screen py-2">
       <Head>
@@ -17,7 +53,22 @@ export default function Home({ pokemons }) {
           Pok&#233;dex
         </h2>
         <SearchBox />
-        <Results pokemons={pokemons} />
+        <Results pokemons={allPokemons} />
+        <button
+          type="button"
+          onClick={() => loadMorePokemons()}
+          className="text-white px-3 py-1 my-3 rounded-md bg-red-500 
+            font-semibold tracking-wide"
+        >
+          Load More
+        </button>
+        <div className="fixed bottom-3 right-5 cursor-pointer">
+          {isVisible && (
+            <div onClick={scrollToTop}>
+              <Image src={scroll} height={50} width={50} />
+            </div>
+          )}
+        </div>
       </main>
     </div>
   );
