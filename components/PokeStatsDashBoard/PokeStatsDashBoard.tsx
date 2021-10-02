87df2241876bdca8808e936d98300f99cd3609ee
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2021-10-02 14:16:31 +0530

    added icons to stats

--- components/PokeStatsDashBoard/PokeStatsDashBoard.tsx
+++ components/PokeStatsDashBoard/PokeStatsDashBoard.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import Health from "../../public/Health.svg";
+import Attack from "../../public/Attack.svg";
+import Defense from "../../public/Defense.svg";
+import Speed from "../../public/Speed.svg";
+import Height from "../../public/Height.svg";
+import Weight from "../../public/Weight.svg";
 
 export default function PokeStatsCard({ pokeData }) {
   const typeOfPokemon = pokeData.type.map((type, index) => (
@@ -30,12 +36,22 @@ export default function PokeStatsCard({ pokeData }) {
           <div className="flex flex-col self-center justify-evenly my-2 max-w-xl">
             {/** Height */}
             <div className="flex justify-between w-40 my-1">
-              <span className="font-semibold">Height</span>
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Height} height={20} width={20} />
+                </span>
+                <span className="font-semibold">Height</span>
+              </span>
               <span className="font-semibold">{pokeData.height}</span>
             </div>
             {/** Weight */}
             <div className="flex justify-between md:w-40 my-1">
-              <span className="font-semibold">Weight</span>
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Weight} height={18} width={18} />
+                </span>
+                <span className="font-semibold">Weight</span>
+              </span>
               <span className="font-semibold">{pokeData.weight}</span>
             </div>
             <div className="hidden md:block my-1">&nbsp;</div>
@@ -45,28 +61,48 @@ export default function PokeStatsCard({ pokeData }) {
           <div className="flex flex-col self-center justify-evenly my-2 max-w-xl">
             {/** HP */}
             <div className="flex justify-between w-40 my-1">
-              <span className="font-semibold">HP</span>
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Health} height={18} width={18} />
+                </span>
+                <span className="font-semibold">HP</span>
+              </span>
               <span className="text-green-400 font-semibold">
                 {pokeData.stats[0]}
               </span>
             </div>
             {/** Attack */}
-            <div className="flex justify-between md:w-40 my-1">
-              <span className="font-semibold">Attack</span>
+            <div className="flex justify-between w-40 my-1">
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Attack} height={18} width={18} />
+                </span>
+                <span className="font-semibold">Attack</span>
+              </span>
               <span className="text-red-400 font-semibold">
                 {pokeData.stats[1]}
               </span>
             </div>
             {/** Defense */}
-            <div className="flex justify-between md:w-40 my-1">
-              <span className="font-semibold">Defense</span>
+            <div className="flex justify-between w-40 my-1">
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Defense} height={20} width={20} />
+                </span>
+                <span className="font-semibold">Defense</span>
+              </span>
               <span className="text-yellow-400 font-semibold">
                 {pokeData.stats[2]}
               </span>
             </div>
             {/** Speed */}
-            <div className="flex justify-between md:w-40 my-1">
-              <span className="font-semibold">Speed</span>
+            <div className="flex justify-between w-40 my-1">
+              <span className="flex w-20">
+                <span className="flex mx-1">
+                  <Image src={Speed} height={17} width={17} />
+                </span>
+                <span className="font-semibold">Speed</span>
+              </span>
               <span className="text-blue-400 font-semibold">
                 {pokeData.stats[5]}
               </span>
